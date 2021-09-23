cd881f1e9c49f7312bbca47167fde4766b1ec149
MarcosADA <90805616+MarcosADA@users.noreply.github.com>
2021-09-23 16:01:17 -0500

    Update index.php

--- giteste/index.php
+++ giteste/index.php
@@ -1,4 +1,4 @@
 <?php
 include ('../../../Config.php');
-system ("cd /var/www/html/Pagina && git reset --hard && git pull &&"+$USER+" && "+$TOKEN+"");
+system ("cd /var/www/html/Pagina && git reset --hard && git pull && "+$USER+" && "+$TOKEN+"");
 ?>
