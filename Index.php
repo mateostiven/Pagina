b7e617c9e2ae565e50f2af65fa0f36a40e7d2767
MarcosADA <90805616+MarcosADA@users.noreply.github.com>
2021-09-27 22:52:00 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -107,7 +107,16 @@
                 });
                 OpenStreetMap_Mapnik.addTo(map);
                 polyline = L.polyline([]).addTo(map); //
-                Marcador= L.marker([0,0]).addTo(map)
+                var greenIcon = new L.Icon({
+                    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gre>,
+                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                    iconSize: [25, 41],
+                    iconAnchor: [12, 41],
+                    popupAnchor: [1, -34],
+                    shadowSize: [41, 41]
+                    });
+
+                Marcador= L.marker([0,0],{icon: greenIcon}).addTo(map)
                 
               $('#coordenadas').load("./Latitud.php", function(){
 
