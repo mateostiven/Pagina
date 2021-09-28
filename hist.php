c7bfb4d7665653099360e57bcdc69cdcffb4a532
pachperdev <pachperdev@gmail.com>
2021-09-28 00:53:33 -0500

    fix boton regresar en hist.php

--- hist.php
+++ hist.php
@@ -36,7 +36,7 @@
         <div id='map'></div>
         </center>
 
-        <form method="POST" action="Index.php">
+        <form method="POST" action="hist.php">
         <br> 
         <center>
           <input type="submit" values="Regresar" class="btn btn-sucess" name="btn1">    
