3bc13ff39db7407d713e103f25c310bfaa4fee0d
pachperdev <pachperdev@gmail.com>
2021-10-04 00:57:15 -0500

    cambios a los titulos

--- skills.php
+++ skills.php
@@ -20,7 +20,7 @@
 
   </script>
 
-  <title>Mini Portfolio | Skills</title>
+  <title>Taxi Service | Real Time</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="css/bootstrap.css" rel="stylesheet">
