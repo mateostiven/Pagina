0fc575727ac4a79e312e1272d51ef87a63109753
pachperdev <pachperdev@gmail.com>
2021-10-07 15:28:13 -0500

    cambio al value - date

--- historic.php
+++ historic.php
@@ -48,12 +48,12 @@
         <form method="POST" action="historic.php">
 
           <label for="start">Desde:</label>
-          <input type="date" id="FechaIn" name="FechaIn" value="2021-09-23" min="2021-01-01" max="2022-12-31">
+          <input type="date" id="FechaIn" name="FechaIn" value="2021-10-07" min="2021-01-01" max="2022-12-31">
 
           <input type="time" id="MinIn" name="MinIn" value="02:30" min="00:00" max="24:00" required>
 
           <label for="appt">Hasta:</label>
-          <input type="date" id="FechaFn" name="FechaFn" value="2021-09-23" min="2021-01-01" max="2022-12-31">
+          <input type="date" id="FechaFn" name="FechaFn" value="2021-10-07" min="2021-01-01" max="2022-12-31">
 
           <input type="time" id="MinFn" name="MinFn" value="23:30" min="00:00" max="24:00" required>
 
